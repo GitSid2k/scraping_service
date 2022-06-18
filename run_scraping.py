e0db95afee8f2ccb035e4113de259ac8e1fc3507
GitSid2k <sidorov2a@gmail.com>
2022-06-18 15:56:26 +0300

    newDB

--- run_scraping.py
+++ run_scraping.py
@@ -1,6 +1,19 @@
 import codecs
+import os
+import sys
+
+from django.db import DatabaseError
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
+
+import django
+
+django.setup()
 
 from scraping.parsers import *
+from scraping.models import Vacancy, City, Language
 
 parsers = (
     (hh,
@@ -8,11 +21,21 @@ parsers = (
     (fl, 'https://www.fl.ru/search/?type=projects&search_string=python&action=search')
 )
 
+city = City.objects.filter(slug='rostov-na-donu').first()
+language = Language.objects.filter(slug='python').first()
+
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
+
+for job in jobs:
+    v = Vacancy(**job, city=city, language=language)
+    try:
+        v.save()
+    except DatabaseError:
+        pass
 
 h = codecs.open('work.txt', 'w', 'utf-8')
 h.write(str(jobs))
